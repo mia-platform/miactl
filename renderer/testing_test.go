ff3b00ee2f74ed620eb8378bfc2ba1fa0eb21272
Davide Bianchi <bianchidavide12@gmail.com>
2020-04-17 00:56:20 +0200

    refactor: remove redundant type from slice

--- renderer/testing_test.go
+++ renderer/testing_test.go
@@ -12,9 +12,9 @@ func TestCleanTableRows(t *testing.T) {
 
 	table := NewTable(buf, []string{"h1", "h2", "h3"})
 	table.AppendBulk([][]string{
-		[]string{"r1c1", "r1c2", "r1c3"},
-		[]string{"r2c1", "r2c2", "r2c3"},
-		[]string{"r3c1", "r3c2", "r3c3"},
+		{"r1c1", "r1c2", "r1c3"},
+		{"r2c1", "r2c2", "r2c3"},
+		{"r3c1", "r3c2", "r3c3"},
 	})
 	table.Render()
 
