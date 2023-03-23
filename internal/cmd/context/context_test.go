a89b2ca9e8c3c1b55832e4149106e3b81e31fd8c
Giorgia Fiscaletti <giorgia.fiscaletti@mia-platform.eu>
2023-03-23 12:57:38 +0100

    test: add tests for context helper functions

--- internal/cmd/context/context_test.go
+++ internal/cmd/context/context_test.go
@@ -16,9 +16,11 @@
 package context
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/mia-platform/miactl/internal/clioptions"
+	"github.com/spf13/viper"
 	"github.com/stretchr/testify/require"
 )
 
@@ -28,4 +30,103 @@ func TestNewContextCmd(t *testing.T) {
 		cmd := NewContextCmd(opts)
 		require.NotNil(t, cmd)
 	})
+}
+
+func TestGetContextBaseURL(t *testing.T) {
+	viper.SetConfigType("yaml")
+
+	// valid config1 and get
+	config1 := `contexts:
+  fake-ctx:
+    apibaseurl: http://url
+    companyid: "123"
+    projectid: "123"`
+	err := viper.ReadConfig(strings.NewReader(config1))
+	if err != nil {
+		t.Fatalf("unexpected error reading config: %v", err)
+	}
+
+	// valid context
+	url, err := GetContextBaseURL("fake-ctx")
+	require.NoError(t, err)
+	require.Equal(t, "http://url", url)
+
+	// wrong context name
+	url, err = GetContextBaseURL("wrong-ctx")
+	require.Error(t, err)
+	require.Equal(t, "", url)
+}
+
+func TestGetContextCompanyID(t *testing.T) {
+	viper.SetConfigType("yaml")
+
+	config1 := `contexts:
+  fake-ctx:
+    apibaseurl: http://url
+    companyid: "123"
+    projectid: "123"`
+	err := viper.ReadConfig(strings.NewReader(config1))
+	if err != nil {
+		t.Fatalf("unexpected error reading config: %v", err)
+	}
+
+	// valid context
+	companyID, err := GetContextCompanyID("fake-ctx")
+	require.NoError(t, err)
+	require.Equal(t, "123", companyID)
+
+	// wrong context name
+	companyID, err = GetContextCompanyID("wrong-ctx")
+	require.Error(t, err)
+	require.Equal(t, "", companyID)
+
+	// company id unset
+	config2 := `contexts:
+  fake-ctx:
+    apibaseurl: http://url
+    companyid: "123"
+    projectid: "123"`
+	err = viper.ReadConfig(strings.NewReader(config2))
+	if err != nil {
+		t.Fatalf("unexpected error reading config: %v", err)
+	}
+
+	companyID, err = GetContextCompanyID("wrong-ctx")
+	require.Error(t, err)
+	require.Equal(t, "", companyID)
+}
+
+func TestGetCurrentContext(t *testing.T) {
+	viper.SetConfigType("yaml")
+
+	// current context set
+	config1 := `contexts:
+  fake-ctx:
+    apibaseurl: http://url
+    companyid: "123"
+    projectid: "123"
+current-context: fake-ctx`
+	err := viper.ReadConfig(strings.NewReader(config1))
+	if err != nil {
+		t.Fatalf("unexpected error reading config: %v", err)
+	}
+
+	currentContext, err := GetCurrentContext()
+	require.NoError(t, err)
+	require.Equal(t, "fake-ctx", currentContext)
+
+	// current context unset
+	config2 := `contexts:
+  fake-ctx:
+    apibaseurl: http://url
+    companyid: "123"
+    projectid: "123"`
+	err = viper.ReadConfig(strings.NewReader(config2))
+	if err != nil {
+		t.Fatalf("unexpected error reading config: %v", err)
+	}
+
+	currentContext, err = GetCurrentContext()
+	require.Error(t, err)
+	require.Equal(t, "", currentContext)
 }
