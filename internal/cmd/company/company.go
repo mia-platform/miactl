510af334467d6583ac081d7b1f129ce4285ead0e
Giorgia Fiscaletti <giorgia.fiscaletti@mia-platform.eu>
2023-04-20 16:08:09 +0200

    feat: add empty company cmd

---
+++ internal/cmd/company/company.go
@@ -0,0 +1,34 @@
+// Copyright Mia srl
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package company
+
+import (
+	"github.com/mia-platform/miactl/internal/clioptions"
+	"github.com/spf13/cobra"
+)
+
+func NewCompanyCmd(options *clioptions.CLIOptions) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "company",
+		Short: "view and manage mia companies",
+	}
+
+	options.AddConnectionFlags(cmd)
+	options.AddContextFlags(cmd)
+	options.AddCompanyFlags(cmd)
+
+	return cmd
+}
