f6c4dbc84682b9b87f2854c21896c19ad09c2912
Federico Maggi <federico.maggi@mia-platform.eu>
2024-06-20 11:29:23 +0200

    fix: missing files

---
+++ internal/cmd/extensibility/cmd.go
@@ -0,0 +1,40 @@
+// Copyright Mia srl
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package extensibility
+
+import (
+	"github.com/mia-platform/miactl/internal/clioptions"
+	"github.com/spf13/cobra"
+)
+
+func CMD(o *clioptions.CLIOptions) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "extensions",
+		Short: "Manage registered extensions",
+		Long:  "Manage registered extensions for the company.",
+	}
+
+	flags := cmd.PersistentFlags()
+	o.AddConnectionFlags(flags)
+	o.AddContextFlags(flags)
+	o.AddCompanyFlags(flags)
+	o.AddProjectFlags(flags)
+
+	cmd.AddCommand(
+		ListCmd(o),
+	)
+	return cmd
+}
