3218f82b414563332e13897e911fd5444637f5b8
Davide Bianchi <bianchidavide12@gmail.com>
2024-06-24 17:56:46 +0200

    fix: suggest only with 1 arg

--- internal/cmd/context/use.go
+++ internal/cmd/context/use.go
@@ -39,7 +39,10 @@ func UseCmd(opts *clioptions.CLIOptions) *cobra.Command {
 			fmt.Printf("Switched to context \"%s\"\n", newContext)
 			return nil
 		},
-		ValidArgsFunction: func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
+		ValidArgsFunction: func(_ *cobra.Command, args []string, _ string) ([]string, cobra.ShellCompDirective) {
+			if len(args) > 0 {
+				return nil, cobra.ShellCompDirectiveNoFileComp
+			}
 			locator := cliconfig.NewConfigPathLocator()
 			config, err := locator.ReadConfig()
 			if err != nil {
