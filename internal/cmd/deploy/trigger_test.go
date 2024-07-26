0a4b629906ed32047e1b9e8a2d9bc63bbd77dc2b
Davide Bianchi <bianchidavide12@gmail.com>
2024-07-26 11:54:10 +0200

    feat: update deploy command to be deploy trigger

--- internal/cmd/deploy/trigger_test.go
+++ internal/cmd/deploy/trigger_test.go
@@ -57,7 +57,7 @@ func TestDeploy(t *testing.T) {
 				ProjectID:    testCase.projectID,
 				MiactlConfig: filepath.Join(t.TempDir(), "nofile"),
 			}
-			err := run(context.TODO(), "environmentName", options)
+			err := runDeployTrigger(context.TODO(), "environmentName", options)
 			if testCase.expectErr {
 				require.Error(t, err)
 				return
